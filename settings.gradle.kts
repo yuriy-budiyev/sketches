41a91907e7a1972ea7ade6e75f91a4221f3272f5
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-12 23:21:26 +0300

    Dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,20 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+    versionCatalogs {
+        create("scratches") {
+            version("kotlin", "1.9.20")
+            version("coroutines", "1.7.3")
+            plugin("android", "com.android.application").versionRef("kotlin")
+        }
     }
 }
 
