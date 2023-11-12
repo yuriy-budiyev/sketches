f84d1effb2d84e461e5310633c60ad6c50f34ff3
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-12 20:32:28 +0300

    Dev

--- app/src/main/java/com/github/yuriybudiyev/sketches/ui/theme/Theme.kt
+++ app/src/main/java/com/github/yuriybudiyev/sketches/ui/theme/Theme.kt
@@ -58,7 +58,10 @@ fun SketchesTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(
+                window,
+                view
+            ).isAppearanceLightStatusBars = darkTheme
         }
     }
 
