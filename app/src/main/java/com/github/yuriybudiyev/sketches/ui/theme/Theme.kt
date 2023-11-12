a9af85f5780cbd8d74f19432cae714ea9c386448
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-12 20:36:09 +0300

    Dev

--- app/src/main/java/com/github/yuriybudiyev/sketches/ui/theme/Theme.kt
+++ app/src/main/java/com/github/yuriybudiyev/sketches/ui/theme/Theme.kt
@@ -15,13 +15,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+private val sketchesDarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
-private val LightColorScheme = lightColorScheme(
+private val sketchesLightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
@@ -50,8 +50,8 @@ fun SketchesTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> sketchesDarkColorScheme
+        else -> sketchesLightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
