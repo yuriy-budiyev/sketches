03442e0ba0c18267f7004a3d5e3a964cdff8bd2f
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-14 12:27:10 +0300

    Dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,9 +14,14 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("scratches") {
+            version("application", "8.1.3")
             version("kotlin", "1.9.20")
             version("coroutines", "1.7.3")
-            plugin("android", "com.android.application").versionRef("kotlin")
+            version("ksp", "1.9.20-1.0.14")
+            plugin("application", "com.android.application").versionRef("application")
+            plugin("kotlin", "org.jetbrains.kotlin.android").versionRef("kotlin")
+            plugin("ksp","com.google.devtools.ksp").versionRef("ksp")
+
         }
     }
 }
