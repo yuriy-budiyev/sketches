bef2d2498ae0d2b9690bc5c4d218d21390e895ac
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-18 22:04:23 +0300

    Dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,26 @@
 pluginManagement {
+
     repositories {
+
         google()
+
         mavenCentral()
+
         gradlePluginPortal()
     }
-
 }
+
 dependencyResolutionManagement {
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
+
         google()
+
         mavenCentral()
     }
+
     versionCatalogs {
 
         create("sketches") {
