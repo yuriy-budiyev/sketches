922481ff06231a46c6b7a03c1cb909602f1f99df
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-20 17:00:11 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/gallery/ui/GalleryActivityViewModel.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/gallery/ui/GalleryActivityViewModel.kt
@@ -1,6 +1,8 @@
 package com.github.yuriybudiyev.sketches.gallery.ui
 
 import androidx.lifecycle.ViewModel
+import com.github.yuriybudiyev.sketches.gallery.model.reository.GalleryRepository
 
-class GalleryActivityViewModel : ViewModel() {
+class GalleryActivityViewModel(private val repository: GalleryRepository) : ViewModel() {
+
 }
