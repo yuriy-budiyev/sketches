d5ec173813f163eb6be58041a6c43689e9f7d82f
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-22 02:33:21 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/theme/Color.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/theme/Color.kt
@@ -26,10 +26,12 @@ package com.github.yuriybudiyev.sketches.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val SketchesPrimary = Color(0xFF6B69D6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SketchesSecondaryLight = Color(0xFF7F7DDB)
+val SketchesTertiaryLight = Color(0xFF9391E1)
+val SketchesBackgroundLight = Color(0xFFFBFBFB)
+
+val SketchesSecondaryDark = Color(0xFF5755D1)
+val SketchesTertiaryDark = Color(0xFF4341CB)
+val SketchesBackgroundDark = Color(0xFF2B2B2B)
--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/theme/Theme.kt
@@ -39,33 +39,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val sketchesDarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val sketchesLightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
 @Composable
 fun SketchesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -95,3 +72,17 @@ fun SketchesTheme(
         content = content
     )
 }
+
+private val sketchesLightColorScheme = lightColorScheme(
+    primary = SketchesPrimary,
+    secondary = SketchesSecondaryLight,
+    tertiary = SketchesTertiaryLight,
+    background = SketchesBackgroundLight
+)
+
+private val sketchesDarkColorScheme = darkColorScheme(
+    primary = SketchesPrimary,
+    secondary = SketchesSecondaryDark,
+    tertiary = SketchesTertiaryDark,
+    background = SketchesBackgroundDark
+)
