0b2aa29c1b269cb4dfc38ba6610e972ecb1db60c
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-27 11:50:04 +0300

    Dev

---
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/gallery/data/reository/implementation/GalleryRepositoryImpl.kt
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Yuriy Budiyev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.yuriybudiyev.sketches.gallery.data.reository.implementation
+
+import android.content.ContentUris
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+import androidx.core.net.toUri
+import com.github.yuriybudiyev.sketches.gallery.data.model.GalleryImage
+import com.github.yuriybudiyev.sketches.gallery.data.reository.GalleryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class GalleryRepositoryImpl(private val context: Context): GalleryRepository {
+
+    override suspend fun getImages(): List<GalleryImage>? {
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } else {
+            context.getExternalFilesDir(null)
+                ?.toUri()
+        } ?: return null
+        val cursor = withContext(Dispatchers.IO) {
+            context.contentResolver.query(
+                uri,
+                arrayOf(
+                    MediaStore.Images.Media._ID,
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.BUCKET_ID,
+                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+                    MediaStore.Images.Media.DATE_ADDED,
+                    MediaStore.Images.Media.WIDTH,
+                    MediaStore.Images.Media.HEIGHT
+
+                ),
+                null,
+                null,
+                "${MediaStore.Images.Media.DATE_ADDED} DESC"
+            )
+        } ?: return null
+        val images = ArrayList<GalleryImage>(cursor.count)
+        val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+        val nameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+        val bucketIdColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_ID)
+        val bucketNameColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
+        val dateAddedColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATE_ADDED)
+        val withColumn = cursor.getColumnIndex(MediaStore.Images.Media.WIDTH)
+        val heightColumn = cursor.getColumnIndex(MediaStore.Images.Media.HEIGHT)
+        while (cursor.moveToNext()) {
+            val id = cursor.getLong(idColumn)
+            images += GalleryImage(
+                id,
+                cursor.getString(nameColumn),
+                cursor.getLong(bucketIdColumn),
+                cursor.getString(bucketNameColumn),
+                cursor.getLong(dateAddedColumn) * 1000L,
+                cursor.getInt(withColumn),
+                cursor.getInt(heightColumn),
+                ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+            )
+        }
+        return images
+    }
+}
