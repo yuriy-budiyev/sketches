b3f31175c06c0359d3d0e1a109ea0b0fbc4ce73a
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-11-28 10:56:02 +0300

    Dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,23 @@
 pluginManagement {
-
     repositories {
-
         google()
-
         mavenCentral()
-
         gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
-
         google()
-
         mavenCentral()
     }
-
     versionCatalogs {
-
         create("sketches") {
-
             from(files("sketches.versions.toml"))
         }
     }
 }
 
 rootProject.name = "sketches"
-
 include(":app")
