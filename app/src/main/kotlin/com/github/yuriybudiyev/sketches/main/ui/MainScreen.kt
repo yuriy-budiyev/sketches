e22d9a68d204153676675207766c95dc7fbf48b2
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-12-08 23:08:45 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/ui/MainScreen.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/ui/MainScreen.kt
@@ -24,3 +24,49 @@
 
 package com.github.yuriybudiyev.sketches.main.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Photo
+import androidx.compose.material.icons.rounded.PhotoLibrary
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+    NavigationBar {
+        NavigationBarItem(selected = true,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.Photo,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            label = {
+                Text(text = "Images")
+            })
+        NavigationBarItem(selected = true,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.PhotoLibrary,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            label = {
+                Text(text = "Buckets")
+            })
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    MainScreen()
+}
