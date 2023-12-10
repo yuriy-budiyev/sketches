8c68dac8a956dff91e98285cf25aa82c4dd2c197
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-12-10 07:44:06 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/navigation/SketchesNavHost.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/navigation/SketchesNavHost.kt
@@ -2,10 +2,9 @@ package com.github.yuriybudiyev.sketches.main.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.github.yuriybudiyev.sketches.images.navigation.IMAGES_ROUTE
-import com.github.yuriybudiyev.sketches.images.ui.ImagesRoute
+import com.github.yuriybudiyev.sketches.images.navigation.imagesScreen
 
 @Composable
 fun SketchesNavHost() {
@@ -14,8 +13,6 @@ fun SketchesNavHost() {
         navController = navController,
         startDestination = IMAGES_ROUTE
     ) {
-        composable(route = IMAGES_ROUTE) {
-            ImagesRoute(onImageClick = {})
-        }
+        imagesScreen { }
     }
 }
