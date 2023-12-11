2df85138b3b4f1f65fbc045350b0392c0579c010
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-12-11 15:04:18 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/buckets/navigation/BucketsNavigation.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/buckets/navigation/BucketsNavigation.kt
@@ -1,18 +1,35 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Yuriy Budiyev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.yuriybudiyev.sketches.buckets.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavController
 import androidx.navigation.NavDeepLink
-import androidx.navigation.NavOptions
 import com.github.yuriybudiyev.sketches.R
 import com.github.yuriybudiyev.sketches.core.navigation.destination.TopLevelNavigationDestination
 import com.github.yuriybudiyev.sketches.core.ui.icon.SketchesIcons
-
-const val BUCKETS_ROUTE = "buckets"
-
-fun NavController.navigateToBuckets(navOptions: NavOptions?) {
-}
 
 object BucketsNavigationDestination: TopLevelNavigationDestination {
 
