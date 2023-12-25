7d957772e1cc6774a6e9891c6ac99211895ab91c
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2023-12-26 01:33:40 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/icon/SketchesIcons.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/icon/SketchesIcons.kt
@@ -1,9 +1,12 @@
 package com.github.yuriybudiyev.sketches.core.ui.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Photo
 import androidx.compose.material.icons.filled.PhotoLibrary
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.VideoFile
 import androidx.compose.material.icons.outlined.BrokenImage
 import androidx.compose.material.icons.outlined.Photo
 import androidx.compose.material.icons.outlined.PhotoLibrary
@@ -16,4 +19,5 @@ object SketchesIcons {
     val BucketsUnselected = Icons.Outlined.PhotoLibrary
     val ImageLoadError = Icons.Outlined.BrokenImage
     val Share = Icons.Filled.Share
+    val Video = Icons.Filled.PlayCircleOutline
 }
