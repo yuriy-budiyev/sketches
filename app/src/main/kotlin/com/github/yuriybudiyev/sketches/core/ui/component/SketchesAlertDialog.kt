b76fcb386a0e67d26b474d0ae81776dea32dfa86
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-11 21:26:57 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/component/SketchesAlertDialog.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/component/SketchesAlertDialog.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SketchesAlertDialog(
@@ -45,7 +46,10 @@ fun SketchesAlertDialog(
             Text(text = titleText)
         },
         text = {
-            Text(text = contentText)
+            Text(
+                text = contentText,
+                fontSize = 16.sp,
+            )
         },
         onDismissRequest = onNegativeResult,
         confirmButton = {
