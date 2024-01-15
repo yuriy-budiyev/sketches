c3e27379cff4dfd3996edc15436f14d9d03be805
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-15 19:27:32 +0300

    Dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
