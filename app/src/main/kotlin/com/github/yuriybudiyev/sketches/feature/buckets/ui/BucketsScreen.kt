290fbc06618e56943ed2fbda746b5eb083667c34
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-16 09:49:26 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/feature/buckets/ui/BucketsScreen.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/feature/buckets/ui/BucketsScreen.kt
@@ -165,7 +165,7 @@ private fun BucketsScreenLayout(
                         .aspectRatio(ratio = 1f)
                         .clip(shape = RoundedCornerShape(8.dp))
                         .border(
-                            width = 0.5.dp,
+                            width = 1.dp,
                             color = MaterialTheme.colorScheme.onBackground,
                             shape = RoundedCornerShape(8.dp),
                         ),
