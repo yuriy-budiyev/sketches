0ef92f2d830cf1551ee8a4eb2b56ae2584083718
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-24 09:51:35 +0300

    Dev

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -44,8 +44,8 @@ android {
 }
 
 baselineProfile {
-    managedDevices += "pixel6Api33"
-    useConnectedDevices = false
+    //managedDevices += "pixel6Api33"
+    useConnectedDevices = true
 }
 
 dependencies {
