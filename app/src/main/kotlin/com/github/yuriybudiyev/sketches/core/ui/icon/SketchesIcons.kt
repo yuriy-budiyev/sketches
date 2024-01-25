d237681e9a442f9b0829f24030c071147c9481b7
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-25 22:22:22 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/icon/SketchesIcons.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/icon/SketchesIcons.kt
@@ -1,14 +1,14 @@
 package com.github.yuriybudiyev.sketches.core.ui.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.VolumeOff
+import androidx.compose.material.icons.automirrored.filled.VolumeUp
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.Photo
 import androidx.compose.material.icons.filled.PhotoLibrary
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material.icons.filled.VolumeOff
-import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.material.icons.outlined.Photo
 import androidx.compose.material.icons.outlined.PhotoLibrary
 import androidx.compose.material.icons.outlined.PlayCircle
@@ -28,6 +28,6 @@ object SketchesIcons {
     val Video = Icons.Outlined.PlayCircle
     val Play = Icons.Filled.PlayArrow
     val Pause = Icons.Filled.Pause
-    val VolumeEnabled = Icons.Filled.VolumeUp
-    val VolumeDisabled = Icons.Filled.VolumeOff
+    val VolumeEnabled = Icons.AutoMirrored.Filled.VolumeUp
+    val VolumeDisabled = Icons.AutoMirrored.Filled.VolumeOff
 }
