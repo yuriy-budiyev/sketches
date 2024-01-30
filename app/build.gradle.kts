72846bd2cc50b7752e1ed11c5a00439e74b9378b
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-01-30 09:14:42 +0300

    Dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,7 +106,7 @@ dependencies {
     androidTestImplementation(sketches.androidx.compose.ui.test.junit)
     androidTestImplementation(sketches.hilt.test)
     kspAndroidTest(sketches.bundles.hilt.compiler)
-    baselineProfile(project(mapOf("path" to ":baselineprofile")))
+    baselineProfile(project(":baselineprofile"))
 }
 
 baselineProfile {
