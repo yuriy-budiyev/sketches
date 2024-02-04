0d90adf67dbf8c4c596763421f78b6f1e74da715
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-02-04 18:37:17 +0300

    Dev

--- app/src/test/kotlin/com/github/yuriybudiyev/sketches/Snapshots.kt
+++ app/src/test/kotlin/com/github/yuriybudiyev/sketches/Snapshots.kt
@@ -30,133 +30,152 @@ import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.runtime.snapshots.StateObject
 import androidx.compose.runtime.snapshots.StateRecord
 import androidx.compose.runtime.snapshots.readable
+import androidx.compose.runtime.snapshots.withCurrent
 import androidx.compose.runtime.snapshots.writable
 
 class Snapshots {
 
     @Test
     fun snapshots() {
-        val size = Size(
-            0,
-            0
+        val range = Range(
+            start = 100,
+            end = 100
         )
         val snapshot1 = Snapshot.takeMutableSnapshot()
         snapshot1.enter {
-            size.width = 100
-            size.height = 50
-            assertEquals(
-                100,
-                size.width
-            )
+            range.start = 50
+            range.end = 100
             assertEquals(
                 50,
-                size.height
+                range.start
+            )
+            assertEquals(
+                100,
+                range.end
             )
         }
         assertEquals(
-            0,
-            size.width
+            100,
+            range.start
         )
         assertEquals(
-            0,
-            size.height
+            100,
+            range.end
         )
         val snapshot2 = Snapshot.takeMutableSnapshot()
         snapshot2.enter {
-            size.width = 25
-            size.height = 75
+            range.start = 25
+            range.end = 75
             assertEquals(
                 25,
-                size.width
+                range.start
             )
             assertEquals(
                 75,
-                size.height
+                range.end
             )
         }
         assertEquals(
-            0,
-            size.width
+            100,
+            range.start
         )
         assertEquals(
-            0,
-            size.height
+            100,
+            range.end
         )
         snapshot1.enter {
-            size.width += 100
-            size.height += 150
+            range.start -= 50
+            range.end += 150
             assertEquals(
-                200,
-                size.width
+                0,
+                range.start
             )
             assertEquals(
-                200,
-                size.height
+                250,
+                range.end
             )
         }
         assertEquals(
-            0,
-            size.width
+            100,
+            range.start
         )
         assertEquals(
-            0,
-            size.height
+            100,
+            range.end
         )
         snapshot1.apply()
         snapshot1.dispose()
         assertEquals(
-            200,
-            size.width
+            0,
+            range.start
         )
         assertEquals(
-            200,
-            size.height
+            250,
+            range.end
         )
         snapshot2.dispose()
         val snapshot3 = Snapshot.takeMutableSnapshot()
         snapshot3.enter {
-            size.width -= 100
-            size.height -= 100
+            range.start -= 150
+            range.end -= 100
             assertEquals(
-                100,
-                size.width
+                -150,
+                range.start
             )
             assertEquals(
-                100,
-                size.height
+                150,
+                range.end
             )
         }
         snapshot3.apply()
         snapshot3.dispose()
         assertEquals(
-            100,
-            size.width
+            -150,
+            range.start
         )
         assertEquals(
-            100,
-            size.height
+            150,
+            range.end
         )
     }
 }
 
-class Size(
-    width: Int,
-    height: Int,
+class Range(
+    start: Int,
+    end: Int,
 ): StateObject {
 
-    var width: Int
-        get() = records.readable(this).width
+    init {
+        require(start <= end) {
+            "Range start should be less than or equal to end ($start, $end)"
+        }
+    }
+
+    var start: Int
+        get() = records.readable(this).start
         set(value) {
+            records.withCurrent { record ->
+                val recordEnd = record.end
+                require(value <= recordEnd) {
+                    "Range start should be less than or equal to end ($value, $recordEnd)"
+                }
+            }
             records.writable(this) {
-                width = value
+                start = value
             }
         }
 
-    var height: Int
-        get() = records.readable(this).height
+    var end: Int
+        get() = records.readable(this).end
         set(value) {
+            records.withCurrent { record ->
+                val recordStart = record.start
+                require(value >= recordStart) {
+                    "Range end should be greater than or equal to start ($recordStart, $value)"
+                }
+            }
             records.writable(this) {
-                height = value
+                end = value
             }
         }
 
@@ -174,7 +193,7 @@ class Size(
     ): StateRecord? {
         val currentRecord = current as StateStateRecord
         val appliedRecord = applied as StateStateRecord
-        return if (currentRecord.width == appliedRecord.width && currentRecord.height == appliedRecord.height) {
+        return if (currentRecord.start == appliedRecord.start && currentRecord.end == appliedRecord.end) {
             currentRecord
         } else {
             null
@@ -182,25 +201,25 @@ class Size(
     }
 
     private var records = StateStateRecord(
-        width,
-        height
+        start,
+        end
     )
 
     private class StateStateRecord(
-        var width: Int,
-        var height: Int,
+        var start: Int,
+        var end: Int,
     ): StateRecord() {
 
         override fun create(): StateRecord =
             StateStateRecord(
-                width,
-                height
+                start,
+                end
             )
 
         override fun assign(value: StateRecord) {
             val record = value as StateStateRecord
-            width = record.width
-            height = record.height
+            start = record.start
+            end = record.end
         }
     }
 }
