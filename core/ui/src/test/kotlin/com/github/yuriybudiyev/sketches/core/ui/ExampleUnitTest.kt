ea893f8da0e10fa7ddd9b864dcf206f7d75dd494
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2024-02-08 07:05:44 +0300

    Revert "Dev"
    This reverts commit f82a212db1c34678cf28c30787f6e4341a82161a.

--- core/ui/src/test/kotlin/com/github/yuriybudiyev/sketches/core/ui/ExampleUnitTest.kt
+++ core/ui/src/test/kotlin/com/github/yuriybudiyev/sketches/core/ui/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.github.yuriybudiyev.sketches.core.ui
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
