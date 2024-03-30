5fbe28368f0c8c8292e171158ae42c0d74779d72
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-03-31 00:19:07 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/common/permissions/Permissions.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/common/permissions/Permissions.kt
@@ -33,21 +33,3 @@ fun Context.checkPermissionGranted(permission: String): Boolean =
         this,
         permission
     ) == PackageManager.PERMISSION_GRANTED
-
-fun Context.checkAllPermissionsGranted(permissions: Array<String>): Boolean {
-    for (permission in permissions) {
-        if (!checkPermissionGranted(permission)) {
-            return false
-        }
-    }
-    return true
-}
-
-fun checkAllPermissionsGranted(permissions: Map<String, Boolean>): Boolean {
-    for ((_, granted) in permissions) {
-        if (!granted) {
-            return false
-        }
-    }
-    return true
-}
