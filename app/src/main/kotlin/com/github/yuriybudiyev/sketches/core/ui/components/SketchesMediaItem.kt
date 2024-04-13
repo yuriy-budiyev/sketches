b2104993e8f990a50c0de8543160e1eaff51d2a4
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-04-13 13:36:04 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/components/SketchesMediaItem.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/components/SketchesMediaItem.kt
@@ -32,8 +32,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.FilterQuality
@@ -54,10 +52,9 @@ fun SketchesMediaItem(
     iconPadding: Dp,
     modifier: Modifier = Modifier,
 ) {
-    val typeUpdated by rememberUpdatedState(type)
     Box(
         modifier = modifier.semantics {
-            testTag = when (typeUpdated) {
+            testTag = when (type) {
                 MediaType.Image -> "media_item_image"
                 MediaType.Video -> "media_item_video"
             }
@@ -72,7 +69,7 @@ fun SketchesMediaItem(
             enableLoadingIndicator = true,
             enableErrorIndicator = true,
         )
-        if (typeUpdated == MediaType.Video) {
+        if (type == MediaType.Video) {
             Box(
                 modifier = Modifier
                     .align(alignment = Alignment.BottomEnd)
