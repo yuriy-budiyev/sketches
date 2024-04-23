02f3ee4348a2a197f490f8f6ceb020cbe2225821
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-04-24 01:15:30 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/common/permissions/media/Media.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/common/permissions/media/Media.kt
@@ -75,6 +75,34 @@ fun Context.checkMediaAccess(): MediaAccess =
         }
     }
 
+@JvmInline
+value class MediaAccessRequestLauncher(private val launcher: ActivityResultLauncher<Array<String>>) {
+
+    fun requestMediaAccess() {
+        launcher.launch(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                    Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            } else {
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            }
+        )
+    }
+}
+
 @Composable
 inline fun rememberMediaAccessRequestLauncher(crossinline onResult: (MediaAccess) -> Unit): MediaAccessRequestLauncher {
     val launcher = rememberLauncherForActivityResult(
@@ -117,32 +145,4 @@ inline fun rememberMediaAccessRequestLauncher(crossinline onResult: (MediaAccess
         },
     )
     return MediaAccessRequestLauncher(launcher)
-}
-
-@JvmInline
-value class MediaAccessRequestLauncher(private val launcher: ActivityResultLauncher<Array<String>>) {
-
-    fun requestMediaAccess() {
-        launcher.launch(
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                arrayOf(
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_VIDEO,
-                    Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
-                )
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                arrayOf(
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_VIDEO
-                )
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-            } else {
-                arrayOf(
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                )
-            }
-        )
-    }
 }
