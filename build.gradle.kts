2f307a7934828cb996f79a4a04f80d5529f86aec
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2025-02-09 15:51:51 +0300

    Dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,20 @@ plugins {
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.hilt) apply false
 }
+
+subprojects {
+    tasks
+        .withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>()
+        .configureEach {
+            compilerOptions {
+                if (project.findProperty("composeCompilerReports") == "true") {
+                    freeCompilerArgs.add("-P")
+                    freeCompilerArgs.add("plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${layout.buildDirectory.asFile.get().absolutePath}/compose_compiler")
+                }
+                if (project.findProperty("composeCompilerMetrics") == "true") {
+                    freeCompilerArgs.add("-P")
+                    freeCompilerArgs.add("plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${layout.buildDirectory.asFile.get().absolutePath}/compose_compiler")
+                }
+            }
+        }
+}
