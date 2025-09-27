dfe723d8e67e04e72f90baf258cc9c635eabcc8d
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2025-09-27 20:00:13 +0300

    Dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,6 @@ android {
         minSdk = 26
         targetSdk = 36
         compileSdk = 36
-        buildToolsVersion = "36.1.0"
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
