01fc27dd4e4ac325c330f5917cabe7b886cf6ff1
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2025-12-02 20:25:28 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/navigation/SketchesNavRoot.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/navigation/SketchesNavRoot.kt
@@ -115,7 +115,7 @@ fun SketchesNavRoot(
             BucketsNavRoute,
         )
     }
-    val initialRoute by rememberUpdatedState(rootRoutes.first())
+    val initialRoute = remember { rootRoutes.first() }
     val navBackStack =
         rememberSaveable(saver = SnapshotStateListSaver()) {
             SnapshotStateList<NavRoute>().apply {
