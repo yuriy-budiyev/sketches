24eb0f7073e5760ec8d90b8faa4bd6d8275bb5a9
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2026-03-05 16:27:13 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/feature/images/ui/ImagesScreenViewModel.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/feature/images/ui/ImagesScreenViewModel.kt
@@ -33,7 +33,6 @@ import com.github.yuriybudiyev.sketches.core.dagger.getValue
 import com.github.yuriybudiyev.sketches.core.data.model.MediaStoreFile
 import com.github.yuriybudiyev.sketches.core.domain.DeleteMediaFilesUseCase
 import com.github.yuriybudiyev.sketches.core.domain.GetMediaFilesUseCase
-import com.github.yuriybudiyev.sketches.core.platform.collections.CollectionsCompat
 import com.github.yuriybudiyev.sketches.core.ui.model.MediaObservingViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -97,9 +96,7 @@ class ImagesScreenViewModel @Inject constructor(
             val files = withContext(ioDispatcher) { getMediaFiles() }
             if (files.isNotEmpty()) {
                 val groups = withContext(defaultDispatcher) {
-                    files.groupByTo(CollectionsCompat.newLinkedHashMap(numMappings = 96)) { file ->
-                        YearMonth.from(file.dateAdded)
-                    }
+                    files.groupBy { file -> YearMonth.from(file.dateAdded)!! }
                 }
                 emit(
                     UiState.Images(
