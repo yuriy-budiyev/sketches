4f46de0778f0ed3514e9b70f2c50925f2a735972
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2026-05-28 20:40:22 +0300

    Dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.github.yuriybudiyev.sketches"
         minSdk = 23
         targetSdk = 37
-        versionCode = 55
-        versionName = "1.0.56"
+        versionCode = 56
+        versionName = "1.0.57"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
