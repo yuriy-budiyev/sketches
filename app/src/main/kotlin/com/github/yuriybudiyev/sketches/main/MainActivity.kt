a9f5a2c30a6153a48f4b54fd3a52f44b4f89a9e5
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2026-09-19 04:30:15 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/MainActivity.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/main/MainActivity.kt
@@ -117,7 +117,7 @@ class MainActivity: ComponentActivity() {
                         splashScreenView.remove()
                     }
                 }
-                splashScreenView.postDelayed(delayInMillis = 500L) {
+                splashScreenView.postDelayed(500L) {
                     animation.start()
                 }
             }
