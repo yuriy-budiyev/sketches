acd64624133f4a0c5cb27d81889c23b26b4de1fb
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2026-09-25 16:06:43 +0300

    Dev

--- app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/components/media/SketchesMediaGrid.kt
+++ app/src/main/kotlin/com/github/yuriybudiyev/sketches/core/ui/components/media/SketchesMediaGrid.kt
@@ -25,12 +25,9 @@
 package com.github.yuriybudiyev.sketches.core.ui.components.media
 
 import android.os.Parcelable
-import androidx.collection.MutableIntIntMap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
@@ -60,15 +57,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.fastCoerceAtLeast
-import androidx.compose.ui.util.fastFirstOrNull
 import com.github.yuriybudiyev.sketches.R
 import com.github.yuriybudiyev.sketches.core.data.model.MediaFile
 import com.github.yuriybudiyev.sketches.core.platform.content.MediaType
-import com.github.yuriybudiyev.sketches.core.platform.log.logDebug
 import com.github.yuriybudiyev.sketches.core.text.capitalizeFirstChar
 import com.github.yuriybudiyev.sketches.core.ui.animation.defaultAnimateItem
-import com.github.yuriybudiyev.sketches.core.ui.animation.defaultAnimationSpec
 import com.github.yuriybudiyev.sketches.core.ui.components.SketchesLazyGrid
 import com.github.yuriybudiyev.sketches.core.ui.dimens.LocalDimens
 import com.github.yuriybudiyev.sketches.core.ui.theme.withHighTransparency
@@ -397,97 +390,7 @@ private fun MediaItem(
  * LazyGrid scroll is retarded as fuck.
  */
 suspend fun LazyGridState.fastAnimateScrollToStart(files: List<MediaFile>) {
-    var headerItem = layoutInfo.visibleItemsInfo.fastFirstOrNull { item ->
-        item.contentType == SketchesMediaGridContentType.Header
-    }
-    val mediaItem = layoutInfo.visibleItemsInfo.fastFirstOrNull { item ->
-        item.contentType == SketchesMediaGridContentType.Media
-    }
-    if (mediaItem == null) {
-        scrollToItem(index = 0)
-        return
-    }
-    val mediaItemSize = when (layoutInfo.orientation) {
-        Orientation.Vertical -> mediaItem.size.height
-        Orientation.Horizontal -> mediaItem.size.width
-    }
-    val viewportSize = when (layoutInfo.orientation) {
-        Orientation.Vertical -> layoutInfo.viewportSize.height
-        Orientation.Horizontal -> layoutInfo.viewportSize.width
-    }
-    val maxSpan = layoutInfo.maxSpan
-    var jumpIndex = (maxSpan * viewportSize / mediaItemSize)
-    val size = files.size
-    if (size == 0) {
-        scrollToItem(index = 0)
-        return
-    }
-    var headerIndex: Int
-    var fileIndex = 0
-    var date = files[0].dateAdded
-    val groups = MutableIntIntMap()
-    while (fileIndex < size) {
-        var groupSize = 0
-        var nextDate = files[fileIndex].dateAdded
-        while (date.year == nextDate.year && date.monthValue == nextDate.monthValue) {
-            groupSize++
-            if (fileIndex + groupSize > size - 1) {
-                break
-            }
-            nextDate = files[fileIndex + groupSize].dateAdded
-        }
-        headerIndex = fileIndex + groups.size
-        groups[headerIndex] = groupSize
-        if (headerIndex >= jumpIndex) {
-            jumpIndex = headerIndex
-            break
-        }
-        date = nextDate
-        fileIndex += groupSize
-    }
-    if (headerItem == null || firstVisibleItemIndex > jumpIndex) {
-        logDebug { "jump 1" }
-        scrollToItem(index = jumpIndex)
-    }
-    if (headerItem == null) {
-        headerItem = layoutInfo.visibleItemsInfo.fastFirstOrNull { item ->
-            item.contentType == SketchesMediaGridContentType.Header
-        }
-    }
-    if (headerItem == null) {
-        scrollToItem(index = 0)
-        return
-    }
-    val headerItemSize = when (layoutInfo.orientation) {
-        Orientation.Vertical -> headerItem.size.height
-        Orientation.Horizontal -> headerItem.size.width
-    }
-    var scrollSize = 0
-    jumpIndex = 0
-    groups.forEach { headerIndex, groupSize ->
-        scrollSize += headerItemSize
-        var remainingMedia = groupSize
-        while (scrollSize <= viewportSize && remainingMedia > 0) {
-            scrollSize += mediaItemSize
-            remainingMedia -= maxSpan
-        }
-        jumpIndex += (groupSize - remainingMedia).fastCoerceAtLeast(0)
-        if (scrollSize >= viewportSize) {
-            return@forEach
-        }
-    }
-    if (firstVisibleItemIndex > jumpIndex) {
-        logDebug { "jump 2" }
-        scrollToItem(index = jumpIndex)
-    }
-    animateScrollBy(
-        value = -scrollSize.toFloat(),
-        animationSpec = defaultAnimationSpec()
-    )
-    logDebug { "-----" }
-    logDebug { jumpIndex }
-    logDebug { scrollSize }
-
+    scrollToItem(index = 0)
     /*var items = layoutInfo.visibleItemsInfo
     if (items.isEmpty()) {
         scrollToItem(index = 0)
